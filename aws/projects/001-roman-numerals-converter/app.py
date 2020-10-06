c1488b154ef8a9e520eba7a0359e2ced0cbbb645
mert <welbeckthomas12@gmail.com>
2020-10-06 14:15:26 +0300

    app.py de düzenleme yapıldı.

--- aws/projects/001-roman-numerals-converter/app.py
+++ aws/projects/001-roman-numerals-converter/app.py
@@ -31,6 +31,6 @@ def result():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    # app.run(host='0.0.0.0', port=80)
+    # app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
     
